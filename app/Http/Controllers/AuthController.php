522a6ba13e24caf74ef63ea9d9962cb5ce14c998
Scoress <scoress.agbalo@epitech.eu>
2024-10-27 19:59:04 +0100

    add mail token feat NB: if user a register auth_token don't set in db

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,37 +7,87 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
 {
     public function login(Request $request): JsonResponse
-    {
-        if (Auth::attempt([
-            "email" => $request->email,
-            "password" => $request->password,
-        ])) {
-            $user = User::find(auth()->user()->id);
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'auth_token' => 'required',
+    ]);
 
-            $success = [
-                "message" => "Login successful",
-                "access_token" => $user->createToken("authToken")->plainTextToken,
-            ];
+    // Recherche de l'utilisateur avec l'email fourni
+    $user = User::where('email', $request->email)->first();
 
-            return response()->json($success, 200);
-        } else {
-            return response()->json(["message" => "Login failed"], 401);
-        }
+    // Vérification de l'utilisateur
+    if (!$user) {
+        return response()->json(["message" => "User not found"], 404);
     }
+
+    // Vérification du mot de passe
+    if (!Hash::check($request->password, $user->password)) {
+        return response()->json(["message" => "Incorrect password"], 401);
+    }
+
+    // Vérification de l'auth_token
+    if ($user->auth_token !== $request->auth_token) {
+        return response()->json(["message" => "Invalid auth token"], 401);
+    }
+
+    // Authentification réussie
+    Auth::login($user);
+
+    $success = [
+        "message" => "Login successful",
+        "access_token" => $user->createToken("authToken")->plainTextToken,
+    ];
+
+    return response()->json($success, 200);
+}
+
 
     public function register(RegisterRequest $request): JsonResponse
     {
         if (Auth::guest()) {
+            // Vérifiez si l'email existe déjà
+            $existingUser = User::where('email', $request->email)->first();
+
+            if ($existingUser) {
+                return response()->json(["message" => "Cet email est déjà utilisé"], 409);
+            }
+
+            $authToken = Str::random(20);
+
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
+                'auth_token' => $authToken,
             ]);
+
+            // Envoi du mail de notification à l'admin
+            Mail::send('emails.adminNotification', [
+                'name' => $user->name,
+                'email' => $user->email,
+                'authToken' => $authToken,
+            ], function ($message) {
+                $message->to('scoress.agbalo@epitech.eu');
+                $message->subject('Nouvelle inscription d’utilisateur');
+            });
+
+            // Envoyer un email de bienvenue à l’utilisateur
+            Mail::send('emails.userWelcome', [
+                'name' => $user->name,
+                'email' => $user->email,
+            ], function ($message) use ($user) {
+                $message->to($user->email);
+                $message->subject('Bienvenue sur notre plateforme');
+            });
 
             $success = ["access_token" => $user->createToken("authToken")->plainTextToken,];
 
